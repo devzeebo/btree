c976f43cfed53fdfbd5ae303878dbb2471f2ec3e
Eric Siebeneich <eric.siebeneich@gmail.com>
2015-05-04 13:01:09 -0500

    BTree extends BTN, fixed adding elements

--- src/main/edu/zeebo/sp15/cosc6390/btree/BTree.groovy
+++ src/main/edu/zeebo/sp15/cosc6390/btree/BTree.groovy
@@ -4,17 +4,43 @@ package edu.zeebo.sp15.cosc6390.btree
  * User: Eric
  * Date: 4/30/2015
  */
-class BTree<K, V> {
-
-	BTreeNode<K> root
+class BTree<K, V> extends BTreeNode<K> {
 
 	BTree() {
-		root = BTreeNode.leaf
+		isLeaf = true
+		size = MAX_ENTRIES
 	}
 
-	def add(K key, V value) {
-		if (!root.add(key, value)) {
-			root = root.split()
+	def split() {
+
+		println "Split Root"
+
+		// create two new children
+		BTreeNode<K> left = isLeaf ? leaf : node
+		BTreeNode<K> right = isLeaf ? leaf : node
+		// assign parent to this
+		[left, right]*.parent = this
+
+		// Assign the left and right pointer lists
+		left.pointers = pointers.subList(0, count / 2 as int) as LinkedList
+		right.pointers = pointers.subList(count / 2 as int, count) as LinkedList
+
+		println "$isLeaf : left: $left.isLeaf -- right: $right.isLeaf"
+		println "${left.pointers.collect { it.key }.join(',')}"
+		println "${right.pointers.collect { it.key }.join(',')}"
+		// reassign the parent node if not leafs
+		if (!isLeaf) {
+			[left, right].each { node -> node.pointers*.value*.parent = node }
+		}
+
+		// add the children to this
+		pointers.clear()
+		[left, right].each { addDirect(new BTreeEntry(it.pointers[0].key, it)) }
+
+		// Transform into a node
+		if (isLeaf) {
+			isLeaf = false
+			size = MAX_KEYS
 		}
 	}
 
@@ -23,6 +49,11 @@ class BTree<K, V> {
 	}
 
 	public static void main(String[] args) {
-		new BTree()
+		BTree<String, String> tree = new BTree<>()
+
+		('a'..'z').each {
+			tree.add(it, it)
+			tree.printTree()
+		}
 	}
 }
--- src/main/edu/zeebo/sp15/cosc6390/btree/BTreeNode.groovy
+++ src/main/edu/zeebo/sp15/cosc6390/btree/BTreeNode.groovy
@@ -18,43 +18,62 @@ class BTreeNode<K> {
 	int size
 
 	def pointers = new LinkedList<BTreeEntry<K, ?>>()
-
+	// The 11th pointer
 	BTreeNode<K> sibling
+
+	BTreeNode parent
 
 	def getCount() { pointers.size() }
 
 	def split() {
-		// Create two new nodes
-		BTreeNode<K> parent = node
+		println "Split"
+		// Create sibling node
 		BTreeNode<K> sib = isLeaf ? leaf : node
 
+		// Assign the parent
+		sib.parent = parent
+
 		// Split our pointer set
-		sib.pointers = pointers.subList(MAX_KEYS / 2 as int, MAX_KEYS) as LinkedList
-		pointers = pointers.subList(0, MAX_KEYS / 2 as int) as LinkedList
+		sib.pointers = pointers.subList(size / 2 as int, pointers.size()) as LinkedList
+		// Reassign the parent
+		if (!isLeaf) {
+			sib.pointers*.value*.parent = sib
+		}
+		pointers = pointers.subList(0, size / 2 as int) as LinkedList
 
 		// Reassign the sibling hierarchy
 		sib.sibling = sibling
 		sibling = sib
 
-		// Add ourselves to our parent
-		parent.pointers << new BTreeEntry<K, Object>(pointers[0].key, this)
-		parent.pointers << new BTreeEntry<K, Object>(sib.pointers[0].key, this)
+		// Add sibling to our parent
+		parent.addDirect(new BTreeEntry(sib.pointers[0].key, sib))
+	}
 
-		return parent
+	def addDirect(BTreeEntry entry) {
+
+		pointers.add(pointers.findLastIndexOf{ it.key < entry.key } + 1, entry)
+
+		if (count > size) {
+			split()
+		}
 	}
 
 	def add(K key, value) {
-		if (isLeaf) {
-			if (count >= size) {
-				return false
-			}
 
-			pointers.add pointers.lastIndexOf{ it.key <= key }, new BTreeEntry(key, value)
+		if (isLeaf) {
+			addDirect(new BTreeEntry(key, value))
 		}
 		else {
-			if (!(pointers.lastIndexOf { it.key <= key } as BTreeNode).add(key, value)) {
+			(pointers[pointers.findLastIndexOf{ it.key <= key }].value as BTreeNode).add(key, value)
+		}
+	}
 
-			}
+	def printTree(int indent = 0) {
+		print '\t' * indent
+		println pointers*.key.join(',')
+
+		if (!isLeaf) {
+			pointers*.value*.printTree(indent + 1)
 		}
 	}
 }
